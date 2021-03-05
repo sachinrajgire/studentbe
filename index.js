d30596a8f0dcc37b63d3f4831ad8edfc43dcc35d
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2021-03-04 22:19:16 -0500

    added new route

--- index.js
+++ index.js
@@ -3,47 +3,46 @@ const app = express()
 const port = process.env.PORT || 4000
 const data = require('./Student_Data.json');
 const { v4: uuidv4 } = require('uuid');
-const querystring = require('querystring');
 var cors = require('cors')
 var path = require('path');
 
 app.use(cors())
 
+// let  uuidData = data.map(i=>{
+//   return {
+//       ...i, 
+//       Id: uuidv4()
+//   }
+//  })
+let  uuidData = data.map(i=>{
+  return {
+      ...i, 
+      Id: uuidv4()
+  }
+ })
+ 
+
+
+app.get('/allrecords', (req, res) => {
+   res.send(uuidData)
+})
+
+app.get('/recorddetails', (req, res) => {
+  console.log(req.query,'req.query')  //mention
+  let filter= uuidData.filter(i=>i.Id === req.query.Id)
+  console.log(filter,'filter');
+   res.send(filter)
+})
+
+// app.get('/recorddetails/:id', (req, res) => {
+//   console.log(req.params,'req.params')  //mention
+
+// })
+
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/send.html'));
 });
-
-app.get('/allrecords', (req, res) => {
-  let  uuidData = data.map(i=>{
-    return {
-        ...i, 
-        Id: uuidv4()
-    }
-    
-    })
-
-  res.send(uuidData)
-})
-app.get('/recorddetails/:id', (req, res) => {
-  
-  console.log(req.params,'req.params')
-
-  
-
-  let  uuidData = data.map(i=>{
-    return {
-        ...i, 
-        Id: uuidv4()
-    }
-    
-    })
-
-  res.send(uuidData)
-})
-
-
-
 
 
 
