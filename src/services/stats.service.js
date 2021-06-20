4c0326e17d89a88c146591b341fca9743f250ccf
Avinash A Murugappan <58096840+Avinash-Murugappan@users.noreply.github.com>
2021-06-20 12:38:23 -0400

    JUP-2 create table with top 10 Employers (Backend)

--- src/services/stats.service.js
+++ src/services/stats.service.js
@@ -5,28 +5,43 @@ const Record = require('../models/record.model');
 
 const getAllRecords = async () => {
   const com = await Record
-  .find({})
-  .sort({_id:-1})
-  .populate('company',['companyName','careerUrl'])
+    .find({})
+    .sort({ _id: -1 })
+    .populate('company', ['companyName', 'careerUrl'])
 
   //* Used foreach to iterate the array and stored the count in another object variable
   let b = {};
-  com.forEach(el=>{
-    b[[el.company["companyName"],el.company["careerUrl"]]]=
-    (b[[el.company["companyName"],el.company["careerUrl"]]] || 0)+1;
+  com.forEach(el => {
+    b[[el.company["companyName"], el.company["careerUrl"]]] =
+      (b[[el.company["companyName"], el.company["careerUrl"]]] || 0) + 1;
   })
 
   //* After storing it in with number of times it occurred, Then sorted the data in descending order
   const sortedData = Object.fromEntries(
-    Object.entries(b).sort(([,a],[,b]) => b-a)
+    Object.entries(b).sort(([, a], [, b]) => b - a)
   );
 
-  return sortedData;
+  //* making it like a comma separated value
+  const ret = Object.entries(sortedData)
+
+  //* getting it into the required format
+  var modif = ret.map((x) => {
+    return {
+      CompanyName: x[0].split(',')[0],
+      CareerUrl: x[0].split(',')[1],
+      Total: x[1],
+    }
+  })
+
+  //* returning only the first 50 records
+  const slicedArray = modif.slice(0, 50);
+
+  return slicedArray;
 };
 
 
 module.exports = {
 
-    getAllRecords,
+  getAllRecords,
 
-  };
+};
