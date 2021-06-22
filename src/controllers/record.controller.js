68c7e9beb9af3ff4db088b52df7889765b67aab2
sh <shaivysh@gmail.com>
2021-06-22 13:39:12 -0700

    Completed cleaning the data

--- src/controllers/record.controller.js
+++ src/controllers/record.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const {recordService } = require('../services');
 const {companyService } = require('../services');
 var faker = require('faker');
-const _ = require('lodash')
+const _ = require('lodash');
 
 
 const createNewRecord = async (req, res) => {
@@ -87,6 +87,15 @@ res.status(httpStatus.CREATED).send(deDupValue);
     res.status(httpStatus.CREATED).send(comp);
  } 
 
+ function getWordCount(wordArr) {
+  let map = {};
+  for(let i = 0; i < wordArr.length; i++) {
+    let word = wordArr[i];
+    map[word] = (map[word] + 1) || 1;
+  }
+  return map;
+}
+
  const getRecordCloud = async (req, res) => {
 
     let records = await recordService.getAllRecords();
@@ -94,18 +103,27 @@ res.status(httpStatus.CREATED).send(deDupValue);
     const jobTitleArr = records.map(i => i.jobTitle);
 
     let str = jobTitleArr.toString();
-    let wordArr = str.split(/[ ,]+/);
-   
-    function getWordCount(wordArr) {
-      let map = {};
-      for(let i = 0; i < wordArr.length; i++) {
-        let word = wordArr[i];
-        map[word] = (map[word] + 1) || 1;
-      }
-      return map;
-    } 
+    let wordArr = str.split(/[ ,]+/); 
 
-    let result = getWordCount(wordArr);
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+    function removePunctuation(str) {
+      let s = str.replace(regex, '');
+      s = s.replace("(", "");
+      s = s.replace(")", "");
+      return s.replace(/[0-9]/g, '');
+    }
+
+    for(let w in wordArr) {
+      removePunctuation(wordArr[w]);
+    }
+
+    let filteredArr = _.difference(wordArr, ["III", "II", "-", "Jr.", "1", "2", "(", ")", "a", "the", "2020", "+", "(Software", 
+    "Engineer)","and", "I", "&", "of", "Of", "(SDE", "2)", "(Tier", "IS", "data", "Jr", "IP", "(NLP)", "(Data", "UA", "Body", "Fleet", 
+    "at", "(Web", "Development)", "In", "Robots", "(Int.)", "\"Manufacturing", "\"", "\"Software","/", "(Fall", "Intern)", "Analyst/", 
+    "(Volunteer)", "Manager/", "Manager:", "Manager-", "President-", "Sr."]);
+
+    let result = getWordCount(filteredArr);
 
     res.status(httpStatus.CREATED).send(result);
  }
